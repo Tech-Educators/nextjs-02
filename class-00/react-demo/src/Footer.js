c1c84311a02e99f1d38cf04194898b9d316c442f
Tim Smith <tim.smith2814@gmail.com>
2023-08-14 13:03:47 +0100

    Classes 00-03

---
+++ class-00/react-demo/src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&#169; Tech Educators</p>
+    </footer>
+  );
+}
---
+++ class-00/react-demo/src/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>NextJS is better than this...</h1>
+    </header>
+  );
+}
