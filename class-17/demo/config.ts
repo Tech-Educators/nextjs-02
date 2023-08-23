8489dd8b69c9681a871df011597b81274feac475
Tim Smith <tim.smith2814@gmail.com>
2023-08-23 14:30:08 +0100

    undo

--- class-17/demo/config.ts
+++ class-17/demo/config.ts
@@ -1,5 +1,4 @@
-// export const WEBSITE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
-export const WEBSITE_URL = `https://${process.env.VERCEL_URL}`;
+export const WEBSITE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
 // export const WEBSITE_URL = "https://fetch-fun.vercel.app"
 // export const WEBSITE_URL = "http://localhost:3000"
